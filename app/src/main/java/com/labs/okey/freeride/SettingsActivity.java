892880b7c8cb42f5593d95d4c2770463a12c1bbb
Oleg Kleiman <oleg_kleyman@hotmail.com>
2015-08-24 13:33:28 +0300

    1.3 - re-assembled fastride structure

--- app/src/main/java/com/labs/okey/freeride/SettingsActivity.java
+++ app/src/main/java/com/labs/okey/freeride/SettingsActivity.java
@@ -83,14 +83,13 @@ public class SettingsActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh_settings) {
+            onRefreshGeofences();
             return true;
+        } else if( id == R.id.action_refresh_classifiers) {
+            onRefreshClassifiers();
         }
 
         return super.onOptionsItemSelected(item);
