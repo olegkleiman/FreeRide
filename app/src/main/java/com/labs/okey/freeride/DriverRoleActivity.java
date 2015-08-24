99fc7cce54f3205b673ffdcbd2c470241444990b
Oleg Kleiman <oleg_kleyman@hotmail.com>
2015-08-25 01:44:19 +0300

    1.4.2 - more precision in project structure

--- app/src/main/java/com/labs/okey/freeride/DriverRoleActivity.java
+++ app/src/main/java/com/labs/okey/freeride/DriverRoleActivity.java
@@ -1,23 +1,98 @@
 package com.labs.okey.freeride;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.AlertDialogWrapper;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.labs.okey.freeride.adapters.WiFiPeersAdapter2;
+import com.labs.okey.freeride.model.Ride;
+import com.labs.okey.freeride.model.WifiP2pDeviceUser;
+import com.labs.okey.freeride.utils.BLEUtil;
+import com.labs.okey.freeride.utils.ClientSocketHandler;
 import com.labs.okey.freeride.utils.Globals;
+import com.labs.okey.freeride.utils.GroupOwnerSocketHandler;
+import com.labs.okey.freeride.utils.IMessageTarget;
+import com.labs.okey.freeride.utils.IRecyclerClickListener;
+import com.labs.okey.freeride.utils.IRefreshable;
+import com.labs.okey.freeride.utils.ITrace;
+import com.labs.okey.freeride.utils.WAMSVersionTable;
+import com.labs.okey.freeride.utils.WiFiUtil;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 
-public class DriverRoleActivity extends BaseActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class DriverRoleActivity extends BaseActivityWithGeofences
+        implements ITrace,
+        IMessageTarget,
+        Handler.Callback,
+        IRefreshable,
+        IRecyclerClickListener,
+        WiFiUtil.IPeersChangedListener,
+        WifiP2pManager.PeerListListener,
+        WifiP2pManager.ConnectionInfoListener,
+        GoogleApiClient.ConnectionCallbacks,
+        WAMSVersionTable.IVersionMismatchListener,
+        ResultCallback<Status> // for geofences' callback
+{
 
     private static final String LOG_TAG = "FR.Driver";
+
+    WiFiPeersAdapter2 mPeersAdapter;
+    public List<WifiP2pDeviceUser> peers = new ArrayList<>();
+
+    WiFiUtil wifiUtil;
 
     TextView mTxtStatus;
     TextView mTxtMonitorStatus;
     RecyclerView mPeersRecyclerView;
+
+    String mUserID;
+    String mCarNumber;
+
+    final int WIFI_CONNECT_REQUEST = 1;// request code for starting WiFi connection
+    // handled  in onActivityResult
+
+    private Handler handler = new Handler(this);
+    public Handler getHandler() {
+        return handler;
+    }
+
+    private MobileServiceTable<Ride> ridesTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +100,124 @@ public class DriverRoleActivity extends BaseActivity {
         setContentView(R.layout.activity_driver_role);
 
         setupUI(getString(R.string.title_activity_driver_role), "");
+        wamsInit(true);
+
+        // Keep device awake when advertising fow Wi-Fi Direct
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        ridesTable = getMobileServiceClient().getTable("rides", Ride.class);
+
+        wifiUtil = new WiFiUtil(this);
+        wifiUtil.deletePersistentGroups();
+
+        List<String> _cars = new ArrayList<>();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Set<String> carsSet = sharedPrefs.getStringSet(Globals.CARS_PREF, new HashSet<String>());
+        if( carsSet != null ) {
+            Iterator<String> iterator = carsSet.iterator();
+            while (iterator.hasNext()) {
+                String carNumber = iterator.next();
+                _cars.add(carNumber);
+            }
+        }
+
+
+        String[] cars = new String[_cars.size()];
+        cars = _cars.toArray(cars);
+
+        if( cars.length == 0 ) {
+            new MaterialDialog.Builder(this)
+                    .title(R.string.edit_car_dialog_caption2)
+                    .content(R.string.edit_car_dialog_text)
+                    .autoDismiss(true)
+                    .cancelable(false)
+                    .positiveText(getString(R.string.edit_car_button_title2))
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            Intent intent = new Intent(getApplicationContext(),
+                                    SettingsActivity.class);
+                            startActivity(intent);
+                        }
+                    })
+                    .show();
+        }else if( cars.length > 1) {
+
+            new MaterialDialog.Builder(this)
+                    .title(R.string.edit_car_dialog_caption1)
+                    .autoDismiss(true)
+                    .cancelable(false)
+                    .items(cars)
+                    .positiveText(getString(R.string.edit_car_button_title))
+                    .itemsCallback(new MaterialDialog.ListCallback() {
+                        @Override
+                        public void onSelection(MaterialDialog dialog,
+                                                View view,
+                                                int which,
+                                                CharSequence text) {
+                            mCarNumber = text.toString();
+                        }
+                    })
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            Intent intent = new Intent(getApplicationContext(),
+                                    SettingsActivity.class);
+                            startActivity(intent);
+                        }
+                    })
+                    .show();
+        } else {
+            mCarNumber = cars[0];
+        }
+
+        mUserID = sharedPrefs.getString(Globals.USERIDPREF, "");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            BLEUtil bleUtil = new BLEUtil(this);
+            Boolean bleRes = bleUtil.startAdvertise();
+        }
+
+        // This will publish the service in DNS-SD and start serviceDiscovery()
+        wifiUtil.startRegistrationAndDiscovery(this, mUserID);
+
+        new Thread() {
+            @Override
+            public void run(){
+
+                try{
+
+                    while (true) {
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                String message = Globals.isInGeofenceArea() ?
+                                        Globals.getMonitorStatus() :
+                                        getString(R.string.geofence_outside);
+
+                                mTxtMonitorStatus.setText(message);
+
+                            }
+                        });
+
+                        Thread.sleep(1000);
+                    }
+                }
+                catch(InterruptedException ex) {
+                    Log.e(LOG_TAG, ex.getMessage());
+                }
+
+            }
+        }.start();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.i(LOG_TAG, "onConfigurationChanged");
     }
 
     @Override
@@ -50,6 +243,11 @@ public class DriverRoleActivity extends BaseActivity {
     }
 
     @Override
+    public void onConnected(Bundle bundle) {
+        super.onConnected(bundle);
+    }
+
+    @Override
     protected void setupUI(String title, String subTitle){
         super.setupUI(title, subTitle);
 
@@ -66,4 +264,231 @@ public class DriverRoleActivity extends BaseActivity {
         Globals.setMonitorStatus(getString(R.string.geofence_outside));
 
     }
+
+    //
+    // ITrace implementation
+    //
+
+    @Override
+    public void trace(final String status) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String current = mTxtStatus.getText().toString();
+                mTxtStatus.setText(current + "\n" + status);
+            }
+        });
+    }
+
+    @Override
+    public void alert(String message, final String actionIntent) {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if( which == DialogInterface.BUTTON_POSITIVE ) {
+                    startActivityForResult(new Intent(actionIntent), WIFI_CONNECT_REQUEST);
+                }
+            }};
+
+        new AlertDialogWrapper.Builder(this)
+                .setTitle(message)
+                .setNegativeButton(R.string.no, dialogClickListener)
+                .setPositiveButton(R.string.yes, dialogClickListener)
+                .show();
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+
+        switch (msg.what) {
+            case Globals.TRACE_MESSAGE:
+                Bundle bundle = msg.getData();
+                String strMessage = bundle.getString("message");
+                trace(strMessage);
+                break;
+
+            case Globals.MESSAGE_READ:
+                byte[] buffer = (byte[] )msg.obj;
+                strMessage = new String(buffer);
+                trace(strMessage);
+                break;
+
+        }
+
+        return true;
+    }
+
+
+    //
+    // Implementation of IRefreshable
+    //
+    public void refresh() {
+        peers.clear();
+        mPeersAdapter.notifyDataSetChanged();
+
+        final ImageButton btnRefresh = (ImageButton)findViewById(R.id.btnRefresh);
+        btnRefresh.setVisibility(View.GONE);
+        final ProgressBar progress_refresh = (ProgressBar)findViewById(R.id.progress_refresh);
+        progress_refresh.setVisibility(View.VISIBLE);
+
+        wifiUtil.startRegistrationAndDiscovery(this, mUserID);
+
+        getHandler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        btnRefresh.setVisibility(View.VISIBLE);
+                        progress_refresh.setVisibility(View.GONE);
+                    }
+                },
+                5000);
+    }
+
+    //
+    // Implementation of IPeerClickListener
+    //
+    public void clicked(View view, int position) {
+
+        try {
+
+            WifiP2pDeviceUser device = peers.get(position);
+
+            if (device.status == WifiP2pDevice.AVAILABLE) {
+                wifiUtil.connectToDevice(device, 0);
+            } else {
+                Toast.makeText(this,
+                        "Device should be in available state",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+        catch(Exception ex){
+            Log.e(LOG_TAG, ex.getMessage());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if( requestCode == WIFI_CONNECT_REQUEST) {
+            // if( resultCode == RESULT_OK ) {
+            // How to distinguish between successful connection
+            // and just pressing back from there?
+            wamsInit(true);
+            //}
+        }
+    }
+
+    //
+    // Implementations of WifiUtil.IPeersChangedListener
+    //
+    @Override
+    public void add(final WifiP2pDeviceUser device) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mPeersAdapter.add(device);
+                mPeersAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    //
+    // Implementation of WifiP2pManager.PeerListListener
+    // Used to synchronize peers statuses after connection
+
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList list){
+        for(WifiP2pDevice device : list.getDeviceList()) {
+            WifiP2pDeviceUser d = new WifiP2pDeviceUser(device);
+            d.setUserId(mUserID);
+            mPeersAdapter.updateItem(d);
+        }
+    }
+
+    //
+    // Implementation of WifiP2pManager.ConnectionInfoListener
+    //
+
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo p2pInfo) {
+        TextView txtMe = (TextView)findViewById(R.id.txtMe);
+        Thread handler = null;
+
+        wifiUtil.requestPeers(this);
+
+         /*
+         * The group owner accepts connections using a server socket and then spawns a
+         * client socket for every client. This is handled by {@code
+         * ServerAsyncTask}
+         */
+        if (p2pInfo.isGroupOwner) {
+            txtMe.setText("ME: GroupOwner, Group Owner IP: " + p2pInfo.groupOwnerAddress.getHostAddress());
+            //new WiFiUtil.ServerAsyncTask(this).execute();
+            try {
+                handler = new GroupOwnerSocketHandler(getHandler());
+                handler.start();
+                trace("Server socket opened.");
+            } catch (IOException e){
+                trace("Failed to create a server thread - " + e.getMessage());
+            }
+        } else {
+            txtMe.setText("ME: NOT GroupOwner, Group Owner IP: " + p2pInfo.groupOwnerAddress.getHostAddress());
+            handler = new ClientSocketHandler(
+                    this.getHandler(),
+                    p2pInfo.groupOwnerAddress,
+                    this,
+                    "!!!Message from DRIVER!!!");
+            handler.start();
+            trace("Client socket opened.");
+        }
+
+        // Optionally may request group info
+        //mManager.requestGroupInfo(mChannel, this);
+
+
+    }
+
+    //
+    // Implementation of IVersionMismatchListener
+    //
+    @Override
+    public void mismatch(int major, int minor, final String url){
+        try {
+            new MaterialDialog.Builder(getApplicationContext())
+                    .title(getString(R.string.new_version_title))
+                    .content(getString(R.string.new_version_conent))
+                    .positiveText(R.string.yes)
+                    .negativeText(R.string.no)
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setData(Uri.parse(url));
+                            //intent.setDataAndType(Uri.parse(url), "application/vnd.android.package-archive");
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    })
+                    .show();
+        } catch (MaterialDialog.DialogException e) {
+            // better that catch the exception here would be use handle to send events the activity
+        }
+    }
+
+    @Override
+    public void match() {
+
+    }
+
+    @Override
+    public void connectionFailure(Exception ex) {
+
+        if( ex != null ) {
+
+            View v = findViewById(R.id.drawer_layout);
+            Snackbar.make(v, ex.getMessage(), Snackbar.LENGTH_LONG);
+        }
+
+    }
+
 }
