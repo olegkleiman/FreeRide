48d3a930e33d92432962bf6b0b6566be3e70bdcd
Oleg Kleiman <oleg_kleyman@hotmail.com>
2015-10-20 05:01:20 +0300

    2.9.7 New eye matching algorithm

--- app/src/main/java/com/labs/okey/freeride/fastcv/FastCVWrapper.java
+++ app/src/main/java/com/labs/okey/freeride/fastcv/FastCVWrapper.java
@@ -17,15 +17,30 @@ public class FastCVWrapper {
     public boolean findTemplate(long matAddrRgba, long matAddrGray, long matAddrTemplate, int rotation) {
         return FindTemplate(mNativeObj, matAddrRgba, matAddrGray, matAddrTemplate, rotation);
     }
-    public boolean matchTemplate(long matAddrRgba, long matAddrGray, int rotation) {
+    public int matchTemplate(long matAddrRgba, long matAddrGray, int rotation) {
         return MatchTemplate(mNativeObj, matAddrRgba, matAddrGray, rotation);
+    }
+    public boolean DetectFace(long matAddrRgba, long matAddrGray,
+                              long matAddrFace,
+                              int rotation){
+        return detectFace(mNativeObj, matAddrRgba, matAddrGray,
+                            matAddrFace,
+                            rotation);
+    }
+    public boolean DetectEye(long matAddrRgba, long matAddrGray, long matAddrEye, int rotation){
+        return detectEye(mNativeObj, matAddrRgba, matAddrGray, matAddrEye, rotation);
     }
 
     // Internal native methods
 
     private native long nativeCreateObject(String faceCascadeFileName, String eyesCascadeFileName);
     private native boolean FindTemplate(long thiz, long matAddrRgba, long matAddrGrey, long matAddrTemplate, int rotation);
-    private native boolean MatchTemplate(long thiz, long matAddrRgba, long matAddrGrey, int rotation);
+    private native int MatchTemplate(long thiz, long matAddrRgba, long matAddrGrey, int rotation);
     private native int DetectFaces(long matAddrRgba, String face_cascade_name);
+
+    private native boolean detectFace(long thiz, long matAddrRgba, long matAddrGrey,
+                                      long matAddrFace,
+                                      int rotation);
+    private native boolean detectEye(long thiz, long matAddrRgba, long matAddrGrey, long matAddrEye, int rotation);
 
 }
