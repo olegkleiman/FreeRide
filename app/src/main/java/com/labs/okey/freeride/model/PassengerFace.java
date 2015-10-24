29e8f21827edfc851f88bd5b33fb088af9033bbf
olegkleiman <oleg_kleyman@hotmail.com>
2015-10-24 05:02:51 +0300

    2.9.8 Driver sefies

--- app/src/main/java/com/labs/okey/freeride/model/PassengerFace.java
+++ app/src/main/java/com/labs/okey/freeride/model/PassengerFace.java
@@ -1,11 +1,14 @@
 package com.labs.okey.freeride.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.UUID;
 
 /**
  * Created by Oleg Kleiman on 8/24/15.
  */
-public class PassengerFace {
+public class PassengerFace implements Parcelable {
 
     private String faceId;
     public void setFaceId(String value) {
@@ -22,4 +25,38 @@ public class PassengerFace {
     public PassengerFace(String _faceId){
         this.faceId = _faceId;
     }
+
+    public PassengerFace() {
+
+    }
+
+    //
+    // Implementation of Parcelable
+    //
+
+    private PassengerFace(Parcel in) {
+        setFaceId( in.readString() );
+        setPictureUrl(  in.readString() );
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(faceId);
+        parcel.writeString(pictureUrl);
+    }
+
+    public static final Parcelable.Creator<PassengerFace> CREATOR = new Parcelable.Creator<PassengerFace>() {
+        public PassengerFace createFromParcel(Parcel in) {
+            return new PassengerFace(in);
+        }
+
+        public PassengerFace[] newArray(int size) {
+            return new PassengerFace[size];
+        }
+    };
 }
