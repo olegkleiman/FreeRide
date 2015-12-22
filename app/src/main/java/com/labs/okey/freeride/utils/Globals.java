5b7ba539763c2b2d056c98b956369a61fb3948f0
olegkleiman <oleg_kleyman@hotmail.com>
2015-12-22 14:12:03 +0200

    4.5

--- app/src/main/java/com/labs/okey/freeride/utils/Globals.java
+++ app/src/main/java/com/labs/okey/freeride/utils/Globals.java
@@ -292,7 +292,8 @@ public class Globals {
         }
     }
 
-    public static long GF_OUT_TOLERANCE = 5000;
+    public static long GF_OUT_TOLERANCE = Globals.LOCATION_UPDATE_MIN_FEQUENCY * 2;
+    public static long LOCATION_UPDATE_MIN_FEQUENCY = 3000;
 
     private static final Object lock3 = new Object();
     private static boolean _REMIND_GEOFENCE_ENTRANCE;
